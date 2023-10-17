cf0001bfe9246b343ba2f0656bde0f1a2ab75962
serega552 <107745966+serega552@users.noreply.github.com>
2023-10-17 19:07:38 +0500

    допил управления

--- Assets/Scripts/Player/CameraMover.cs
+++ Assets/Scripts/Player/CameraMover.cs
@@ -6,9 +6,11 @@ public class CameraMover : MonoBehaviour
 {
     [SerializeField] private Vector3 _offSet;
     [SerializeField] private Transform _player;
+    [SerializeField] private float _speed = 5f;
 
     private void FixedUpdate()
     {
-        transform.position = _player.position + _offSet;
+        Vector3 newCamPosition = new Vector3(_player.position.x + _offSet.x, _player.position.y + _offSet.y, _player.position.z + _offSet.z);
+        transform.position = Vector3.Lerp(transform.position, newCamPosition, _speed * Time.deltaTime);
     }
 }
