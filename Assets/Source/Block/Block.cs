42bff85c72931556c7eb7b58699e9e37a1d8cfb5
DaniAndry <danilenkoandrey92@gmail.com>
2024-01-10 15:28:58 +0400

    New models update

--- Assets/Source/Block/Block.cs
+++ Assets/Source/Block/Block.cs
@@ -4,14 +4,8 @@ public class Block : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _crashParticle;
 
-    private Vector3 _spawnPosition;
-    private Quaternion _spawnRotation;
-
     private void OnCollisionEnter(Collision collision)
     {
-        //_spawnPosition = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
-        //_spawnRotation = new Quaternion(transform.rotation.x -50f, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-
         if (collision.gameObject.TryGetComponent(out PlayerMoverView player))
         {
             player.Crash();
