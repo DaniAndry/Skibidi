c95f2185249dbff52ac89ab0e82b5c4a44ecace5
serega552 <serezha-or@mail.ru>
2024-04-02 21:54:08 +0500

    fix

--- Assets/Source/Audio/AudioManager.cs
+++ Assets/Source/Audio/AudioManager.cs
@@ -3,43 +3,47 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-	public static AudioManager Instance;
+    public static AudioManager Instance;
 
-	[SerializeField] private Sound[] _sounds;
+    [SerializeField] private Sound[] _sounds;
 
-	private void Start ()
-	{
-		if (Instance != null)
-		{
-			Destroy(gameObject);
-			return;
-		}
-		else
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
-		foreach (Sound sond in _sounds)
-		{
-			sond.source = gameObject.AddComponent<AudioSource>();
-			sond.source.playOnAwake = false;
-			sond.source.clip = sond.clip;
-			sond.source.volume = sond.volume;
-			sond.source.pitch = sond.pitch;
-			sond.source.loop = sond.loop;
-		}
-	}
+        foreach (Sound sond in _sounds)
+        {
+            sond.source = gameObject.AddComponent<AudioSource>();
+            sond.source.playOnAwake = false;
+            sond.source.clip = sond.clip;
+            sond.source.volume = sond.volume;
+            sond.source.pitch = sond.pitch;
+            sond.source.loop = sond.loop;
+        }
+    }
 
-	public void Play(string sound)
-	{
+    public void Play(string sound)
+    {
         Sound s = Array.Find(_sounds, item => item.name == sound);
-		s.source.Play();
-	}
 
-	public void Stop(string sound)
-	{
-		Sound s = Array.Find(_sounds, item => item.name == sound);
-		s.source.Stop();
-	}
+        if (s != null)
+            s.source.Play();
+    }
+
+    public void Stop(string sound)
+    {
+        Sound s = Array.Find(_sounds, item => item.name == sound);
+
+        if (s != null)
+            s.source.Stop();
+    }
 }
