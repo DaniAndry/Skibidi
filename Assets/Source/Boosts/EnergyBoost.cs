56e253e3377925198ccef046b200096d80f4503d
serega552 <serezha-or@mail.ru>
2024-05-15 17:36:32 +0500

    newReward

--- Assets/Source/Boosts/EnergyBoost.cs
+++ Assets/Source/Boosts/EnergyBoost.cs
@@ -1,4 +1,25 @@
-using UnityEngine;
+using System;
 
 public class EnergyBoost : Boost
-{ }
+{
+    private void OnEnable()
+    {
+        AwardGiver.OnReward += GiveRewardBoost;
+    }
+
+    private void OnDisable()
+    {
+        AwardGiver.OnReward -= GiveRewardBoost;
+    }
+
+    private void GiveRewardBoost(string name, int amount)
+    {
+        if (name == Convert.ToString(ResourceType.EnergyBoost))
+        {
+            for (int i = 0; i <= amount; i++)
+            {
+                Increase();
+            }
+        }
+    }
+}
